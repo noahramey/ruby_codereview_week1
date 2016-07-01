4b9e5e420798ac3d2b5879215d0f58c8503d8516
Noah Ramey <noahramey1997@gmail.com>
2016-07-01 08:50:01 -0700

    add my embedded ruby pages and app.rb is finished

---
+++ app.rb
@@ -0,0 +1,15 @@
+require("sinatra")
+require("sinatra/reloader")
+require("./lib/word_count")
+also_reload "lib/**/*.rb"
+
+get("/") do
+  erb(:index)
+end
+
+get("/sentence") do
+  @sentence = params.fetch("sentence")
+  @word = params.fetch("word")
+  @results = @sentence.word_count(@word)
+  erb(:results)
+end
