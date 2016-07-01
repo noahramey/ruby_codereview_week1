bc3526dd8be69f02d276e065f314e285b288a70f
Noah Ramey <noahramey1997@gmail.com>
2016-07-01 08:38:10 -0700

    add a few more specs for special cases

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -8,6 +8,12 @@ class String
         total += 1
       end
     end
+    if total == 0
+      total = "Your word is not in the sentence you inputted."
+    else
+      total
+    end
+    
     total
   end
 end
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -2,7 +2,15 @@ require('rspec')
 require('word_count')
 
 describe('String#word_count') do
-  it ("takes a phrase and a specific word from the user and identifies how many times that word has been used in the phrase") do
+  it("takes a phrase and a specific word from the user and identifies how many times that word has been used in the phrase") do
     expect("If Peter Piper picked a peck of pickled peppers, where's the peck of pickled peppers Peter Piper picked?".word_count("peck")).to(eq(2))
+  end
+
+  it("identifies the number of times a number is repeated in a phrase as well") do
+    expect("Peter had 2 dogs, 2 cats, and 2 gerbils.".word_count('2')).to(eq(3))
+  end
+
+  it("returns the phrase 'Your word is not in the sentence you inputted.' if the word is not in the sentence") do
+    expect("Peter had 2 dogs, 2 cats, and 2 gerbils.".word_count('horse')).to(eq('Your word is not in the sentence you inputted.'))
   end
 end
